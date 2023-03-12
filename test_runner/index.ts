2109d32bed6d52bef4c1e8712b5a4c24b4f7251a
Glowman554 <glowman434@gmail.com>
2023-03-12 04:06:30 +0100

    added color to unit tests

--- test_runner/index.ts
+++ test_runner/index.ts
@@ -22,6 +22,9 @@ async function runCommand(command: string) {
 	return stdout;
 }
 
+const COLOR_RESET = "\u001B[0m";
+const RED = "\u001B[31m";
+const GREEN = "\u001B[32m";
 
 async function runTest(file: string) {
 	try {
@@ -67,16 +70,16 @@ async function runTest(file: string) {
 
 		if (passed) {
 			summary += "=== TEST PASSED ===";
-			console.log("TEST PASSED: " + file);
+			console.log(`${GREEN}TEST PASSED:${COLOR_RESET} ${file}`);
 		} else {
 			summary += "=== TEST NOT PASSED ===";
-			console.log("TEST NOT PASSED: " + file);
+			console.log(`${RED}TEST NOT PASSED:${COLOR_RESET} ${file}`);
 		}
 
 
 		Deno.writeTextFileSync(file + ".summary", summary);
 	} catch (e) {
-		console.log("RUN FAILED FOR: " + file + ". Reason: " + e);
+		console.log(RED + "RUN FAILED FOR: " + file + ". Reason: " + e + COLOR_RESET);
 	}
 }
 async function main() {
