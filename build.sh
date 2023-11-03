e5d3ca29d96ae6a7fd1226e11b3c7b1e2658c929
Glowman554 <Glowman554@users.noreply.github.com>
2023-11-03 19:41:44 +0000

    deploy: 3574720e54521c8d923f518f50dd78231caf21dc

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -ex
 deno bundle compiler.ts > compiler.js
-mkdir -p res
-cp ../../stdlib ./res/ -rfv
-cp ../../tests/powers_of_two.fl ./res/example.fl
+# mkdir -p res
+# cp ../../stdlib ./res/ -rfv
+# cp ../../tests/powers_of_two.fl ./res/example.fl
