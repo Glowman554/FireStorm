5533148d67bad9171c322db0777d9b8e34754786
Glowman554 <glowman434@gmail.com>
2024-05-22 00:12:34 +0200

    added deletion stuff

--- remote/api.go
+++ remote/api.go
@@ -27,6 +27,46 @@ func CreatePackage(ctx context.Context, name string) error {
 	return err
 }
 
+//encore:api auth method=GET path=/package/delete/full/:name
+func DeletePackage(ctx context.Context, name string) error {
+	uid, ok := auth.UserID()
+	if !ok {
+		return errors.New("missing uid")
+	}
+
+	pkg, err := loadPackage(ctx, name)
+	if err != nil {
+		return err
+	}
+
+	if pkg.Owner != string(uid) {
+		return errors.New("you are not allowed to delete this package")
+	}
+	return deletePackage(ctx, name)
+}
+
+type DeletePackageVersionProps struct {
+	Version string `json:"version"`
+}
+
+//encore:api auth method=GET path=/package/delete/version/:name
+func DeletePackageVersion(ctx context.Context, name string, props *DeletePackageVersionProps) error {
+	uid, ok := auth.UserID()
+	if !ok {
+		return errors.New("missing uid")
+	}
+
+	pkg, err := loadPackage(ctx, name)
+	if err != nil {
+		return err
+	}
+
+	if pkg.Owner != string(uid) {
+		return errors.New("you are not allowed to delete this package version")
+	}
+	return deletePackageVersion(ctx, name, props.Version)
+}
+
 type UploadFileProps struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
@@ -55,6 +95,43 @@ func UploadFile(ctx context.Context, pkgName string, props *UploadFileProps) err
 		Version: props.Version,
 		Content: props.Content,
 	})
+}
+
+type ListFilesProps struct {
+	Version string `json:"version"`
+}
+type ListFilesResponse struct {
+	Files []string `json:"files"`
+}
+
+//encore:api public method=GET path=/package/list/:pkgName
+func ListFiles(ctx context.Context, pkgName string, props *ListFilesProps) (*ListFilesResponse, error) {
+	files, err := loadVersionFileList(ctx, props.Version, pkgName)
+	if err != nil {
+		return nil, err
+	}
+	return &ListFilesResponse{
+		Files: files,
+	}, nil
+}
+
+type LoadFileProps struct {
+	Version string `json:"version"`
+	Name    string `json:"name"`
+}
+type LoadFileResponse struct {
+	Content string `json:"content"`
+}
+
+//encore:api public method=GET path=/package/load/file/:pkgName
+func LoadFile(ctx context.Context, pkgName string, props *LoadFileProps) (*LoadFileResponse, error) {
+	content, err := loadFile(ctx, props.Name, props.Version, pkgName)
+	if err != nil {
+		return nil, err
+	}
+	return &LoadFileResponse{
+		Content: *content,
+	}, nil
 }
 
 // F250UwKglwNE6WJOOlV4db_OG-ONvy1MB06ceH1MKbDCTolAOBbgzVwG3AgmLf9BcMUMRSnvSTLWImxh3syp81W4J1IKhnTBUOzgQudu7MSGPXyE_-WdhlQ6a_XMOLAbhH6ioA
--- remote/database.go
+++ remote/database.go
@@ -41,7 +41,41 @@ func deletePackagesFrom(ctx context.Context, username string) error {
 	return err
 }
 
+func deletePackage(ctx context.Context, pkg string) error {
+	_, err := db.Exec(ctx, "delete from packages where package = $1", pkg)
+	return err
+}
+
 func createFile(ctx context.Context, file File) error {
 	_, err := db.Exec(ctx, "insert into files (name, version, content, package) values ($1, $2, $3, $4)", file.Name, file.Version, file.Content, file.Package.Package)
+	return err
+}
+
+func loadVersionFileList(ctx context.Context, version string, pkg string) ([]string, error) {
+	rows, err := db.Query(ctx, "select name from files where version = $1 and package = $2", version, pkg)
+	if err != nil {
+		return nil, err
+	}
+
+	result := []string{}
+	for rows.Next() {
+		var entry string
+		err = rows.Scan(&entry)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, entry)
+	}
+
+	return result, nil
+}
+
+func loadFile(ctx context.Context, name string, version string, pkg string) (*string, error) {
+	var content *string
+	return content, db.QueryRow(ctx, "select content from files where name = $1 and version = $2 and package = $3", name, version, pkg).Scan(&content)
+}
+
+func deletePackageVersion(ctx context.Context, pkg string, version string) error {
+	_, err := db.Exec(ctx, "delete from files where package = $1 and version = $2", pkg, version)
 	return err
 }
